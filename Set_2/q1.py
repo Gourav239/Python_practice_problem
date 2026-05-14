f2e61bcab96e27f96eb0022077e86966ae918cae
Gourav239 <gourav23905@gmail.com>
2026-05-14 20:48:10 +0530

    Submit Set 2

---
+++ Set_2/q1.py
@@ -0,0 +1,31 @@
+'''
+1. Write a python program to add two numbers.
+2. Write a python program to find remainder when a number is divided by z.
+3. Check the type of variable assigned using input() function.
+4. Use comparison operator to find out whether ‘a’ given variable is greater than ‘b’ or not.
+    Take a = 34 and b = 80
+5. Write a python program to find an average of two numbers entered by the user.
+6. Write a python program to calculate the square of a number entered by the user.
+'''
+
+# a=34
+# b=80
+
+# sum=a+b
+# print(sum)
+
+# rem=a%b
+# print(rem)
+
+# name=input("Enter your name : ")
+# print(type(name))
+
+# print(a>b)
+
+# v1=int(input("Enter value of a : "))
+# v2=int(input("Enter value of b : "))
+# avg=(v1+v2)/2
+# print(avg)
+
+n=int(input("Enter a number : "))
+print(n**2)
